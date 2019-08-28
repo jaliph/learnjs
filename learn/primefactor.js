83093d546d3d5f68918570b3bed83094aa1a0527
Akash Chandra <akachan1@in.ibm.com>
2019-08-28 14:50:03 +0530

    Create primefactor.js

---
+++ learn/primefactor.js
@@ -0,0 +1,20 @@
+const primeFactors = (n) => {
+  let primes = []
+
+  while (n % 2 === 0) {
+    primes.push(2)
+    n = n / 2
+  }
+  for (let i = 3; i * i <= n; i = i + 2) {
+    if (n % i === 0) {
+      primes.push(i)
+      n = n / i
+    }
+  }
+  if (n > 2) {
+    primes.push(n)
+  }
+  return primes
+}
+
+console.dir(primeFactors(45))
