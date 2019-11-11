25aad995db9edad231a704a7e816f4952922fbd2
Akash Chandra <akachan1@in.ibm.com>
2019-11-11 11:49:17 +0530

    Added javascript read from line

---
+++ learn/readfromstdin.js
@@ -0,0 +1,37 @@
+// start processing user input
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+// declare global variables
+let inputStdin = ''
+let inputStdinArray = ''
+let inputCurrentline = 0
+// standard input is stored into inputStdin
+process.stdin.on('data', function (data) {
+  inputStdin += data
+})
+// standard input is done and stored into an array
+// then main is called so that you can start processing your data
+process.stdin.on('end', function () {
+  inputStdinArray = inputStdin.split('\n')
+  main()
+})
+// reads a line from the standard input array
+function readLine () {
+  return inputStdinArray[inputCurrentline++]
+}
+/// //////////// ignore above this line ////////////////////
+// your function that can assist you with solving a problem
+function solveMeFirst (a, b) {
+  // Hint: Type return a+b below
+
+}
+
+function main () {
+  // write your code here.
+  // call `readLine()` to read a line.
+  // use console.log() to write to stdout
+  var a = parseInt(readLine())
+  var b = parseInt(readLine())
+  var res = solveMeFirst(a, b)
+  console.log(res)
+}
