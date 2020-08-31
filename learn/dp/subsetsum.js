340a783fe8901b1c8510239f783a100c8dfd10b0
Avi <avarora@cisco.com>
2020-08-31 16:21:33 +0530

    subset issue

--- learn/dp/subsetsum.js
+++ learn/dp/subsetsum.js
@@ -3,42 +3,59 @@ const recursionSS = (arr, i, sum) => {
   if (sum === 0) return true
   if (i === 0) return false
 
-  if (arr[i - 1] > sum) {
-    return recursionSS(arr, i - 1, sum)
+  if (arr[i-1] > sum) {
+    return recursionSS(arr, i-1, sum)
   }
-  let ith_Index_taken = recursionSS(arr, i - 1, sum - arr[i - 1])
-  let ith_Index_not_taken =  recursionSS(arr, i - 1, sum)
+  let ith_Index_taken = recursionSS(arr, i-1, sum - arr[i-1])
+  let ith_Index_not_taken =  recursionSS(arr, i-1, sum)
   return ith_Index_taken || ith_Index_not_taken
 }
 
 // REVIEW
 const pureDP = (arr, sum) => {
-  const table = Array(arr.length + 1).fill(Array(sum + 1).fill(null))
+  //let table = Array(arr.length + 1).fill(Array(sum + 1).fill(1))
+
+  let len = arr.length;
+
+  //let table = Array(len + 1).fill(Array(sum+1).fill(0));
+  let table = Array.from(Array(len+1), () => new Array(sum+1));
 
   // for sum 0 possible true
-  for (let i = 0; i <= arr.length; i++) {
-    table[i][0] = true
-  }
-  // for no element selected all is false
-  for (let j = 1; j <= sum; j++) {
-    table[0][j] = false
+  for (let i = 0; i<=len; i++) {
+    table[i][0] = true;
   }
 
-  for (let i = 1; i <= arr.length; i++) {
-    for (let j = 1; j <= sum; j++) {
-      // if (j < arr[i - 1]) { // current sum is smaller the current element, dnt select it
-      //   table[i][j] = table[i - 1][j]
+  // for no element selected all is false
+  for (let j = 1; j<=sum; j++) {
+    table[0][j] = false;
+  }  
+
+  console.dir(table); 
+ 
+
+  for (let i = 1; i<=len; i++) {
+
+    for (let j = 1; j<=sum; j++) {
+
+      // if (j < arr[i-1]) { // current sum is smaller the current element, dnt select it
+      //   table[i][j] = table[i-1][j]
       // }
-      if (j >= arr[i - 1]) {
-        table[i][j] = table[i - 1][j - arr[i - 1]] || table[i - 1][j]
-      }
+
+      if (j >= arr[i-1]) {
+        table[i][j] = table[i-1][j - arr[i-1]] || table[i-1][j] ;
+    } else if (j < arr[i-1]) {
+      table[i][j] = table[i-1][j];
+    }
       // table[i][j] = table[i][j - 1]
       // if (i >= arr[j - 1]) table[i][j] = table[i][j] || table[i - arr[j - 1]][j - 1]
     }
-  }
-  console.dir(table)
-  return table[arr.length][sum]
+
+ }
+  //console.dir(table)
+  return table[len][sum];
 }
 
-console.log(pureDP([3, 34, 4, 12, 5, 2], 9))
-console.log(recursionSS([3, 34, 4, 12, 5, 2], 6, 91))
+let arr = [3, 34, 4, 12, 5, 2];
+let sum = 13;
+console.log(pureDP(arr, sum));
+//console.log(recursionSS([3, 34, 4, 12, 5, 2], 6, 91))
