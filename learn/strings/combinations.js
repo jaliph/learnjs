d2a3621f5795e478f7d317c954733bb6694323c3
Akash Chandra <akachan1@in.ibm.com>
2022-08-26 17:55:35 +0530

    Added Tree Code

--- learn/strings/combinations.js
+++ learn/strings/combinations.js
@@ -53,43 +53,65 @@ const find_subsets2 = function(nums) {
 };
 
 
-console.dir(find_subsets2([1, 3]))
+// console.dir(find_subsets2([1, 3]))
 
 
-const combinations = (N) => {
-  let set = [[]]
-  let result = []
-  for (let i = 1 ; i <= N; i++) {
-    let len = set.length
-    for (let j = 0; j < len; j++) {
-      let clone = set[j].slice(0)
-      clone.push(i)
-      if (clone.length == 16) {
-        result.push(clone.slice(0))
-        continue
-      }
-      set.push(clone)
-    }
-  }
-  return result
-}
+// const combinations = (N) => {
+//   let set = [[]]
+//   let result = []
+//   for (let i = 1 ; i <= N; i++) {
+//     let len = set.length
+//     for (let j = 0; j < len; j++) {
+//       let clone = set[j].slice(0)
+//       clone.push(i)
+//       if (clone.length == 2) {
+//         result.push(clone.slice(0))
+//         continue
+//       }
+//       set.push(clone)
+//     }
+//   }
+//   return result
+// }
 
 // console.log(combinations(20))
 
 
-const combinations2 = (arr) => {
+const combinations2 = (arr, k) => {
   let results = []
   const combinationsRecur = (array, index, result) => {
-    console.log(result)
-    if (index >= array.length) {
-      return 
+    if (result.length == k) {
+      results.push(result)
+      return
     }
-  
-    combinationsRecur(array, index + 1, result)
-    combinationsRecur(array, index + 1, [...result, array[index]])
-  }
-  combinationsRecur(arr, 0, results)
+    if (index >= array.length) {
+      return
+    }
+
+    combinationsRecur(array, index + 1, result) // if i is not selected
+    combinationsRecur(array, index + 1, [...result, array[index]]) // if i is selected
+    }
+  combinationsRecur(arr, 0, [])
   return results
 }
 
-console.log(combinations2([1, 2, 3, 4]))
+console.log(combinations2([1, 2, 3, 4], 2))
+
+// const combine = function (arr, k) {
+//   const result = []
+//   const aux = (arr, index = 0, current = []) => {
+//     if (current.length === k) {
+//       result.push(current)
+//       return
+//     }
+//     if (index >= arr.length) {
+//       return
+//     }
+//     aux(arr, index + 1, [...current, arr[index]])
+//     aux(arr, index + 1, current)
+//   }
+//   aux(arr)
+//   return result
+// }
+
+// console.log(combine([1,2,3,4], 2))
