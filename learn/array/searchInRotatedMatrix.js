32ecb61c9f91518b09c216321af5f616bdf1e840
Akash Chandra <akachan1@in.ibm.com>
2022-09-14 09:43:48 +0530

    Added Basic Codes

---
+++ learn/array/searchInRotatedMatrix.js
@@ -0,0 +1,30 @@
+
+
+let binarySearchRotated = function(nums, target) {
+  let low = 0
+  let high = nums.length - 1
+  while(low <= high) {
+    let mid = low + Math.floor((high - low) / 2)
+    
+    if (nums[mid] == target) return mid
+    
+    // the first part is sorted
+    if (nums[low] <= nums[mid]) {
+      if (nums[low] <= target && target < nums[mid]) {
+        high = mid - 1
+      } else {
+        low = mid + 1
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[high]) {
+        low = mid + 1
+      } else {
+        high = mid - 1
+      }
+    }
+  }
+  return -1;
+};
+
+
+// console.log(binarySearchRotated)
