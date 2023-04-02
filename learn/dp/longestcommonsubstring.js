9089f76d63b10d5687519c3583335c5cd61bbc2a
Akash Chandra <akachan1@in.ibm.com>
2023-04-02 11:02:25 +0530

    Update longestcommonsubstring.js

--- learn/dp/longestcommonsubstring.js
+++ learn/dp/longestcommonsubstring.js
@@ -50,4 +50,65 @@ const driver = () => {
   method3([...'TESTJAVA'], [...'JAVA'])
 }
 
-driver()
+// driver()
+
+//// Revise
+
+const lcsLengthBrute = (str1, str2) => {
+  const lcsLengthRecur = (str1, str2, i, j, count) => {
+    if (i >= str1.length || j >= str2.length) {
+      return count
+    }
+
+    let c1 = count, c2 = 0, c3 = 0
+    if (str1[i] == str2[j]) {
+      c1 = lcsLengthRecur(str1, str2, i + 1, j + 1, count + 1)
+    }
+    c2 = lcsLengthRecur(str1, str2, i + 1, j, 0)
+    c3 = lcsLengthRecur(str1, str2, i, j + 1, 0)
+
+    return Math.max(c1, c2, c3)
+  }
+
+  return lcsLengthRecur(str1, str2, 0, 0, 0)
+}
+
+const lcsLength = (str1, str2) => {
+  const dp = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0))
+
+
+  let result = 0
+  for (let i = 1; i <= str1.length ; i++) {
+    for (let j = 1; j <= str2.length ; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+        result = Math.max(result, dp[i][j])
+      } else {
+        dp[i][j] = 0
+      }
+    }
+  }
+  return result
+}
+
+
+// Driver code
+function main() {
+  var s1 = ["educative", "bcdcdcd", "arefun", "yourocks", "abc"];
+  var s2 = ["education", "aacdcdcd", "isfun", "youawesome", "def"];
+
+  // Let's uncomment this to see the benefit of using dynamic programming with memoization
+  // s1.push("ypzrvyigwdiqrnbglvviozqzruvmwivgvqvrfhqi");
+  // s2.push("wdiqrnbglvviozqzruvmwivgvqvrfhqiypzrvyigwdiqrn");
+
+  for (var i = 0; i < s1.length; i++) {
+      console.log(i + 1 + ".\tInput string 1: \"" + s1[i] + "\"");
+      console.log("\tInput string 2: \"" + s2[i] + "\"");
+      console.log("\n\tThe Length of Longest Common Substring is: " + lcsLength(s1[i], s2[i]));
+      console.log("-".repeat(100));
+  }
+}
+
+main();
+
+
