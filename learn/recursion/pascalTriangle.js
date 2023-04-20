67267d665f8f41f92f1a59387b2587cc447aa759
Akash Chandra <akash.chandra.4u@gmail.com>
2023-04-20 11:34:07 -0700

    Added Tabular approach

--- learn/recursion/pascalTriangle.js
+++ learn/recursion/pascalTriangle.js
@@ -7,6 +7,7 @@ function printPascal (num) {
     /// recursion to go to basic levels
     const previous = printPascal(num - 1)
 
+    // console.log(previous)
     // for each previous level, get the new nums in the lines
     for (let i = 0; i < previous.length - 1; i++) {
       lines.push(previous[i] + previous[i + 1])
@@ -19,4 +20,32 @@ function printPascal (num) {
   }
 }
 
-console.log(printPascal(5))
+// console.log(printPascal(5))
+
+
+const pascalTriangleDp = (num) => {
+  const dp = Array(num).fill(1)
+  const result = []
+  result.push(dp)
+  for (j = num;  j > 0; j--) { // run fot num times
+    const prev = result[result.length - 1]
+    const clone = prev.slice(1)  // reduce the new length
+    // console.log('Before')
+    // console.log(prev)
+    // console.dir(clone)
+    for (let i = 0; i < clone.length; i++) {
+      clone[i] = (clone[i - 1] || 0) + prev[i]
+    }
+    // console.dir(dp)
+    // console.log('After')
+    // console.dir(clone)
+    result.push(clone)
+  }
+
+  console.log('final')
+  Print2D(result)
+}
+
+const Print2D = arr => arr.forEach(o => console.log(...o))
+
+pascalTriangleDp(10)
