4559e82d92f4ca5620d00fdb204ae9448899ac5d
Akash Chandra <akash.chandra.4u@gmail.com>
2023-04-20 21:21:58 -0700

    Added DP Code

---
+++ learn/codingMinutes/dp&Recur/friendsPairingProblem.js
@@ -0,0 +1,21 @@
+
+
+const friendPairing = (n) => {
+  const dp = Array(n+1).fill(0)
+
+  for (let i = 0; i <= n; i++) {
+    if (i <= 2) {
+      dp[i] = i
+    } else {
+      dp[i] = dp[i - 1] + (i - 1) * dp[i - 2]
+    }
+  }
+
+  return dp[n]
+}
+
+const main = () => {
+  console.log('Number ways of pairing n friends is ', friendPairing(3))
+}
+
+main()
---
+++ learn/codingMinutes/dp&Recur/orderedSet.js
@@ -0,0 +1,37 @@
+
+
+const orderedSet = (num) => {
+  // base case
+  if (num == 0) return 1
+
+  // recur case
+  let ans = 0
+  for (let i = 1; i <= num; i++) {
+    ans += orderedSet(num - i)
+  }
+
+  return ans
+}
+
+const orderedSetDP = (num) => {
+  const dp = Array(num).fill(0)
+
+  dp[0] = 1
+  let sum = 0
+  for (let i = 1; i <= num; i++) {
+    // sum represents the solution to get the previous num
+    // i represents the number itself which can form the i
+    dp[i] = sum + 1
+    sum += dp[i]
+  }
+
+  console.dir(dp)
+  return dp[num]
+}
+
+const main = () => {
+  console.log('number of ways to order n nums is:', orderedSet(3))
+  console.log('number of ways to order n nums is:', orderedSetDP(3))
+}
+
+main()
